94fd98b4d5cea08e7516b53575f1e18bdb87445e
M.John <john.m@arcor.de>
2025-01-05 20:20:17 +0100

    Create Exception.php

---
+++ src/Exception.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Exception.php
+ *
+ * @since       2024-02-21
+ * @category    Library
+ * @package     Barcoder
+ * @author      
+ * @copyright   2024
+ * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
+ * @link        https://github.com/mjohndus/Barcoder
+ *
+ * This file is part of tc-lib-barcode software library.
+ */
+
+namespace Com\Tecnick\Barcode;
+
+/**
+ * Barcoder\Exception
+ *
+ * Custom Exception class
+ *
+ * @since       2024-02-21
+ * @category    Library
+ * @package     Barcoder
+ * @author      
+ * @copyright   2024 mjohndus
+ * @license     http://www.gnu.org/copyleft/lesser.html GNU-LGPL v3 (see LICENSE.TXT)
+ * @link        https://github.com/mjohndus/Barcoder
+ */
+class Exception extends \Exception
+{
+}
